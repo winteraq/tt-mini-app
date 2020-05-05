5af0569e604364f941f38b2edf4bf068524404da
zhangqian.aq <zhangqian.aq@bytedance.com>
2020-05-05 22:54:46 +0800

    add npmrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,25 @@
 module.exports = {
-    "root": true,
-    "globals": {
-        "getApp": false,
-        "getCurrentPages": false,
-        "Page": false,
-        "Component": false,
-        "App": false,
-        "wx": false,
-        "Behavior": false
-    },
-    "parser": "babel-eslint",
-    "plugins": [
-        "import"
-    ],
-    "rules": {
-        "indent": [2, 4, { "SwitchCase": 2 }],
-        "quotes": [2, "single"],
-        "strict": [2, "never"],
-        "comma-dangle": [2, "always-multiline"],
-        "array-bracket-newline": [2, "consistent"]
-    }
+  "root": true,
+  "globals": {
+    "getApp": false,
+    "getCurrentPages": false,
+    "Page": false,
+    "Component": false,
+    "App": false,
+    "wx": false,
+    "Behavior": false
+  },
+  "parser": "babel-eslint",
+  "plugins": [
+    "prettier",
+    "import"
+  ],
+  "rules": {
+    "indent": [2, 4, {"SwitchCase": 2}],
+    "prettier/prettier": "error",
+    "quotes": [2, "single"],
+    "strict": [2, "never"],
+    "comma-dangle": "off",
+    "array-bracket-newline": [2, "consistent"]
+  }
 }
